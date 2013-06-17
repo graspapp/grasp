33364a679b89d3c1108f343687e460659f126927
Zach Latta <zchlatta@gmail.com>
2013-06-16 21:00:06 -0700

    Create static pages spec and set up routes.

---
+++ spec/features/static_pages_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  subject { page }
+
+  describe "Home page" do
+
+    before { visit root_path }
+
+    it { should have_selector('h1', text: 'Grasp') }
+    it { should have_title('Grasp') }
+  end
+
+  describe "Help page" do
+    
+    before { visit help_path }
+
+    it { should have_selector('h1', text: 'Help') }
+    it { should have_title('Help | Grasp') }
+  end
+
+  describe "About page" do
+    
+    before { visit about_path }
+
+    it { should have_selector('h1', text: 'About') }
+    it { should have_title('About | Grasp') }
+  end
+
+  describe "Contact page" do
+    
+    before { visit contact_path }
+
+    it { should have_selector('h1', text: 'Contact') }
+    it { should have_title('Contact | Grasp') }
+  end
+end
