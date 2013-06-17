1d9af25df1faaa647b6e7b0c2a3b25d577b95344
Zach Latta <zchlatta@gmail.com>
2013-06-16 20:45:42 -0700

    Generate StaticPages controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Grasp::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/help"
+  get "static_pages/about"
+  get "static_pages/contact"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
