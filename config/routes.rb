33bcbaec0de306a91aeae39e69abc57e067dce46
Zach Latta <zchlatta@gmail.com>
2013-06-16 20:59:36 -0700

    Set up static pages routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Grasp::Application.routes.draw do
-  get "static_pages/home"
+  
+  root to: "static_pages#home"
+
   get "static_pages/help"
   get "static_pages/about"
   get "static_pages/contact"
