e7312a3f717a2f0a5e490168f912c49a303e802a
Zach Latta <zchlatta@gmail.com>
2013-06-16 18:08:36 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Grasp::Application.config.secret_key_base = '09769cda2ad9bbaa8b56df8d970d4ab8e46e18a812ab9162fcbd1b1e79d16d681a3e3e7850638e78a93f57becbd52d06109114c74ef7e6e63cfc27574c91eb86'
