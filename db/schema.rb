a77ce4e36f64e5ef4e32330d1384581cac24d1b3
Stanley Jackson <stanjackson6@gmail.com>
2013-06-25 22:23:08 -0700

    Added abilities.rb, and role migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130622193259) do
+ActiveRecord::Schema.define(version: 20130626051252) do
+
+  create_table "role_users", id: false, force: true do |t|
+    t.integer "role_id"
+    t.integer "user_id"
+  end
+
+  create_table "roles", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "first_name"
