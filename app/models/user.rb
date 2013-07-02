16ef97a3955ae07bb2b18cdd9ec2647df1025857
Stanley Jackson <stanjackson6@gmail.com>
2013-07-01 19:12:56 -0700

    User model test suite

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,11 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_and_belongs_to_many :roles
 
+  validates_presence_of :first_name, :last_name, :email, :password, :password_confirmation
+
+
      def role?(role)
       return !!self.roles.find_by_name(role.to_s)
     end 	
+
 end
