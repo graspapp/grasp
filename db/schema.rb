269bf20eef0261874579c6e0c9273c678eb020be
Zach Latta <zchlatta@gmail.com>
2013-07-05 02:03:41 -0700

    Run database migrations and update Gemfile.lock.

--- db/schema.rb
+++ db/schema.rb
@@ -70,8 +70,8 @@ ActiveRecord::Schema.define(version: 20130702154107) do
   add_index "teachers", ["reset_password_token"], name: "index_teachers_on_reset_password_token", unique: true
 
   create_table "users", force: true do |t|
-    t.string   "first_name",             default: "", null: false
-    t.string   "last_name",              default: "", null: false
+    t.string   "first_name"
+    t.string   "last_name"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
