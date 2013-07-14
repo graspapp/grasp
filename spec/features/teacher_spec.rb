f7485f58104cda872343bba411929094c2a5715d
Stanley Jackson <stanjackson6@gmail.com>
2013-07-14 11:04:17 -0700

    Refactored specs. Fixes #17.

--- spec/features/teacher_spec.rb
+++ spec/features/teacher_spec.rb
@@ -1,16 +1,10 @@
 require 'spec_helper'
 
 describe "Teacher" do
-  before do
-    @teacher = Teacher.new(first_name: "Sample", last_name: "Teacher",
-                           email: "sampleteach@school.edu",
-                           password: "fakepass",
-                           password_confirmation: "fakepass",
-                           school: "Sample HS", city: "LA", state: "CA",
-                           country: "United States")
-  end
-
-  subject { @teacher }
+  
+  let(:teacher) { FactoryGirl.create(:teacher) }
+  
+  subject { teacher }
 
   it { should respond_to(:first_name) }
   it { should respond_to(:last_name) }
@@ -23,52 +17,52 @@ describe "Teacher" do
   it { should respond_to(:country) }
 
   describe "when first name is blank" do
-    before { @teacher.first_name = "" }
+    before { teacher.first_name = "" }
     it { should_not be_valid}
   end
 
   describe "when last name is blank" do
-    before { @teacher.last_name = "" }
+    before { teacher.last_name = "" }
     it { should_not be_valid }
   end
   
   describe "when email is blank" do
-    before { @teacher.email = "" }
+    before { teacher.email = "" }
     it { should_not be_valid }
   end
 
   describe "when password is blank" do
-    before { @teacher.password = "" }
+    before { teacher.password = "" }
     it { should_not be_valid }
   end
 
   describe "when password confirmation is blank" do
-    before { @teacher.password_confirmation = "" }
+    before { teacher.password_confirmation = "" }
     it { should_not be_valid }
   end
 
   describe "when school is blank" do
-    before { @teacher.school = "" }
+    before { teacher.school = "" }
     it { should_not be_valid }
   end
 
     describe "when city is blank" do
-    before { @teacher.city = "" }
+    before { teacher.city = "" }
     it { should_not be_valid }
   end
 
     describe "when state is blank" do
-    before { @teacher.state = "" }
+    before { teacher.state = "" }
     it { should_not be_valid }
   end
 
     describe "when country confirmation is blank" do
-    before { @teacher.country = "" }
+    before { teacher.country = "" }
     it { should_not be_valid }
   end
 
   describe "when password is too short" do
-    before { @teacher.password = @teacher.password_confirmation = "short" }
+    before { teacher.password = teacher.password_confirmation = "short" }
     it { should_not be_valid }
   end
   
@@ -77,8 +71,8 @@ describe "Teacher" do
       addresses = %w[teacher@teacher.com dummy.teacher@teacher.org
                      dummy_teacher@teacher.org dummy+teacher@teacher.co.uk]
       addresses.each do |a|
-        @teacher.email = a
-        @teacher.should be_valid
+        teacher.email = a
+        teacher.should be_valid
       end
     end
     
@@ -86,16 +80,22 @@ describe "Teacher" do
       addresses = %w[teacher@teacher,com teacher_at_teacher_dot_org
                      teacher@teacher.]
       addresses.each do |a|
-        @teacher.email = a
-        @teacher.should_not be_valid
+        teacher.email = a
+        teacher.should_not be_valid
       end
     end
   end 
   
   describe "when email address is in use" do
-    before do
-      new_teacher = @teacher.dup
-      new_teacher.save
+
+  let(:teacher2) do
+    FactoryGirl.build(:teacher, email: teacher.email)
+  end
+  
+  subject { teacher2 }
+  
+  before do
+    teacher2.save
   end
 
   it { should_not be_valid }
