6aa0d0ce279e615e500f7ebd45ee5e30a00500d0
Zach Latta <zchlatta@gmail.com>
2013-08-02 01:25:06 -0700

    Create acknowlegements page.

--- Guardfile
+++ Guardfile
@@ -3,7 +3,7 @@ require 'active_support/inflector'
 guard 'spork', :cucumber_env => { 'RAILS_ENV' => 'test' }, 
 
   :rspec_env => { 'RAILS_ENV' => 'test' } do
-  watch('config/application.rb')
+  watch('config/application/rb')
   watch('config/environment.rb')
   watch('config/environments/test.rb')
   watch(%r{^config/initializers/.+\.rb$})
--- config/routes.rb
+++ config/routes.rb
@@ -23,9 +23,9 @@ Grasp::Application.routes.draw do
     delete "/sign_out", to: "sessions#destroy"
   end
   
-
-  get "/help",     to: "static_pages#help"
-  get "/about",    to: "static_pages#about"
-  get "/contact",  to: "static_pages#contact"
-  get "/sign_up",  to: "static_pages#sign_up"
+  get "/help",             to: "static_pages#help"
+  get "/about",            to: "static_pages#about"
+  get "/contact",          to: "static_pages#contact"
+  get "/acknowledgements", to: "static_pages#acknowledgements"
+  get "/sign_up",          to: "static_pages#sign_up"
 end
