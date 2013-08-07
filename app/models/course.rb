03dba3171ab29f80545a8c99b4497a5b9eccf061
Zach Latta <zchlatta@gmail.com>
2013-08-07 13:36:50 -0700

    Generate course codes. Closes #34.

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,10 +1,16 @@
 class Course < ActiveRecord::Base
 
+  after_initialize :init
+
   belongs_to :teacher
 
   has_many :units
   has_many :enrollments
   has_many :students, through: :enrollments
 
-  validates_presence_of :name
+  validates_presence_of :name, :code
+
+  def init
+    self.code ||= SecureRandom.base64(6).gsub(/=+$/,'')
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130802193922) do
+ActiveRecord::Schema.define(version: 20130807012724) do
 
   create_table "courses", force: true do |t|
     t.integer  "teacher_id"
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "code"
   end
 
   create_table "enrollments", force: true do |t|
