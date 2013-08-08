a1524d840462293e2d2a70f67ec0662ea8fce87a
Stanley Jackson <stanjackson6@gmail.com>
2013-08-07 18:14:36 -0700

    Removed migrations

--- db/migrate/20130802193651_add_name_to_learning_targets.rb
+++
@@ -1,5 +0,0 @@
-class AddNameToLearningTargets < ActiveRecord::Migration
-  def change
-    add_column :learning_targets, :name, :string
-  end
-end
--- db/migrate/20130802193922_add_number_to_learning_targets.rb
+++
@@ -1,5 +0,0 @@
-class AddNumberToLearningTargets < ActiveRecord::Migration
-  def change
-    add_column :learning_targets, :number, :string
-  end
-end
