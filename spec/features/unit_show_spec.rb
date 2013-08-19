81b0abba20a841b3c6808166142c4aa52c75f65d
Zach Latta <zchlatta@gmail.com>
2013-08-18 21:23:48 -0700

    Add a page header to the unit's show view.

--- spec/features/unit_show_spec.rb
+++ spec/features/unit_show_spec.rb
@@ -15,7 +15,21 @@ describe "Unit home" do
 
   subject { page }
 
-  it "should have the unit's name as the page title" do
-    should have_title(@unit.name)
+  describe "persistent attributes" do
+
+    it "should have the unit's name as the page title" do
+      should have_title(@unit.name)
+    end
+
+    it "should include the unit's number and name in the heading" do
+      should have_content("Unit #{ @unit.number } - #{ @unit.name }")
+    end
+  end
+
+
+  describe "when logged in as a teacher" do
+  end
+
+  describe "when logged in as a student" do
   end
 end
