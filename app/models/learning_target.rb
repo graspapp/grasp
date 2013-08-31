f341d4913c0220d835d9ddab0e4dc36ca3d0dc20
Zach Latta <zchlatta@gmail.com>
2013-08-31 02:55:25 -0700

    Remove level from learning targets.

--- app/models/learning_target.rb
+++ app/models/learning_target.rb
@@ -1,15 +1,7 @@
 class LearningTarget < ActiveRecord::Base
 
-  MAX_LEVEL = 4
-
-  after_initialize :init
-
   belongs_to :unit
   has_many :tasks, dependent: :destroy
 
-  validates_presence_of :description, :number, :level
-
-  def init
-    self.level ||= 1
-  end
+  validates_presence_of :description, :number
 end
