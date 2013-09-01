794f4e68a7f48cc9f7576811c60fa560e0944ded
Zach Latta <zchlatta@gmail.com>
2013-09-01 13:03:36 -0700

    Validate presence of learning_target_id and level in LearningTargetProgress.

--- app/models/learning_target_progress.rb
+++ app/models/learning_target_progress.rb
@@ -1,4 +1,6 @@
 class LearningTargetProgress < ActiveRecord::Base
 
   belongs_to :enrollment
+  
+  validates_presence_of :learning_target_id, :level
 end
--- spec/models/learning_target_progress_spec.rb
+++ spec/models/learning_target_progress_spec.rb
@@ -2,14 +2,26 @@ require 'spec_helper'
 
 describe LearningTargetProgress do
 
-  before do
-    @learning_target_progress = FactoryGirl.create(:learning_target_progress)
-  end
+  let (:ltp) { FactoryGirl.create(:learning_target_progress) }
+
+  subject { ltp }
 
   describe "attributes" do
 
     it { should respond_to :learning_target_id }
     it { should respond_to :level }
     it { should belong_to  :enrollment }
+  end
+
+  describe "with no learning_target_id" do
+
+    before { ltp.learning_target_id = nil }
+    it { should_not be_valid }
+  end
+
+  describe "with no level" do
+
+    before { ltp.level = nil }
+    it { should_not be_valid }
   end
 end
