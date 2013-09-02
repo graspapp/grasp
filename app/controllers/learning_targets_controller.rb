ca9051310b0e0eab706fb64dc1e0b73a1310cfc6
Zach Latta <zchlatta@gmail.com>
2013-09-02 08:37:40 -0700

    Remove method to level up a learning target.

--- app/controllers/learning_targets_controller.rb
+++ app/controllers/learning_targets_controller.rb
@@ -17,12 +17,4 @@ class LearningTargetsController < ApplicationController
   def lt_params
     params.require(:learning_target).permit(:description)
   end
-
-  # TODO: Remove this
-  def level_up
-    lt = LearningTarget.find(params[:id])
-    lt.level += 1 if lt.level < 4
-    lt.save
-    render :nothing => true  
-  end
 end
