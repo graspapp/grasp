ea6c007da9f8a2b2e2443ac51f7cb96f3d28b028
Zach Latta <zchlatta@gmail.com>
2013-09-03 00:24:06 -0700

    LearningTargets have_many LearningTargetProgresses

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130902225919) do
+ActiveRecord::Schema.define(version: 20130903072049) do
 
   create_table "courses", force: true do |t|
     t.integer  "teacher_id"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20130902225919) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "learning_target_progress_id"
   end
 
   create_table "roles", force: true do |t|
