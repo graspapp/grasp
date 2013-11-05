1c007ffb4e5e9794190521e0b1a703ee8be8c3c2
Zach Latta <zach@zachlatta.com>
2013-11-05 02:42:53 -0800

    Refactor concept progress creation login into a separate method.

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -10,19 +10,7 @@ class UnitsController < ApplicationController
     @unit = Unit.find(params[:id])
 
     if student_signed_in?
-      student = current_student
-      enrollment = Enrollment.where(student_id: student.id,
-                                    course_id: @unit.course_id).first
-
-      @unit.concepts.each do |c|
-        progress = ConceptProgress.where(enrollment_id: enrollment.id,
-                                         concept_id: c.id).first
-
-        if progress.nil?
-          ConceptProgress.create(enrollment_id: enrollment.id,
-                                 concept_id: c.id)
-        end
-      end
+      create_needed_concept_progresses_for current_student
     end
   end
   
@@ -35,9 +23,26 @@ class UnitsController < ApplicationController
       
     redirect_to unit_path(params[:current_unit])
   end
+
+  private
   
   def unit_params
     params.require(:unit).permit(:name)
+  end
+
+  def create_needed_concept_progresses_for(student)
+    enrollment = Enrollment.where(student_id: student.id,
+                                  course_id: @unit.course_id).first
+
+    @unit.concepts.each do |c|
+      progress = ConceptProgress.where(enrollment_id: enrollment.id,
+                                       concept_id: c.id).first
+
+      if progress.nil?
+        ConceptProgress.create(enrollment_id: enrollment.id,
+                               concept_id: c.id)
+      end
+    end
   end
 
   def concept_progress_for(concept, student)
