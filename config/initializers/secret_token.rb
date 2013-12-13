a4e5e0a19935edc89ab794fb3ade81dd372a4658
Zach Latta <zach@zachlatta.com>
2013-12-13 00:05:53 -0800

    Generate new project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Grasp::Application.config.secret_key_base = '482be2bbc0f6af0ac322dfc8042272e23a22c176336ca5d8ae7d4194ef4ecaadae907cde3a88cfc0385fb6ba225c0f9ce46f1475b94ac1832a53bc70552ca5c1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Grasp::Application.config.session_store :cookie_store, key: '_grasp_session'
