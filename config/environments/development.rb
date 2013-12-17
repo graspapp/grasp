e3cc0e7405df9358e5f87a5e1375d174cafb6115
Zach Latta <zach@zachlatta.com>
2013-12-16 19:23:05 -0800

    Install devise.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ Grasp::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # Set the default_url used by ActionMailer
+  config.action_mailer.default_url_options = { host: "localhost:3000" }
 end
