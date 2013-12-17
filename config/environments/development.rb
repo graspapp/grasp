b10f2413b37ba24a55f202a1555f9bd41cb391f2
Stanley Jackson <stanjackson6@gmail.com>
2013-12-16 17:04:44 -0800

    ENV… should not be in quotes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,8 +36,8 @@ Grasp::Application.configure do
      :address => 'smtp.sendgrid.net',
      :port => '587',
      :authentication => :plain,
-     :user_name => 'ENV['SENDGRID_USERNAME']',
-     :password => 'ENV['SENDGRID_PASSWORD']',
+     :user_name => ENV['SENDGRID_USERNAME'],
+     :password => ENV['SENDGRID_PASSWORD'],
      :domain => 'heroku.com',
      :enable_starttls_auto => true
   }
