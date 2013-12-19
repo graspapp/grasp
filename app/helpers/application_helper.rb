744953639506aa74d61662088ec65746978ea744
Zach Latta <zach@zachlatta.com>
2013-12-19 05:11:45 -0800

    Remove devise error message overriding.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,37 +3,4 @@ module ApplicationHelper
     base_title = "Grasp"
     page_title.empty? ? base_title : "#{ page_title } | #{ base_title }"
   end
-
-  def my_devise_error_messages!
-    return "" if resource.errors.empty? && resource.role.errors.empty?
-
-    messages = role_messages = ""
-
-    if !resource.errors.empty?
-      messages = resource.errors.full_messages.map { |msg|
-        content_tag(:li, msg)
-      }.join
-    end
-
-    if !resource.role.errors.empty?
-      role_messages = resource.role.errors.full_messages.map { |msg|
-        content_tag(:li, msg)
-      }.join
-    end
-
-    messages = messages + role_messages   
-    sentence = I18n.t("errors.messages.not_saved",
-                      count: resource.errors.count +
-                        resource.role.errors.count,
-                      resource: resource.class.model_name.human.downcase)
-
-    html = <<-HTML
-    <div id="error_explanation">
-    <h2>#{sentence}</h2>
-    <ul>#{messages}</ul>
-    </div>
-    HTML
-
-    html.html_safe
-  end
 end
