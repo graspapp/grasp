9854ad5e9947a325e0c39957bd757573b1904d4e
Zach Latta <zach@zachlatta.com>
2014-01-02 19:29:01 -0800

    No idea what's causing that deprecated error.

--- config/application.rb
+++ config/application.rb
@@ -19,8 +19,5 @@ module Grasp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    
-    # Enforce available locales and raise an error if there's an issue.
-    config.i18n.enforce_available_locales = true
   end
 end
