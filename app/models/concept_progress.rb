ff3cd96c813ef7cb6c4eb138f7893d615c4b1017
Zach Latta <zach@zachlatta.com>
2014-01-08 00:26:48 -0800

    Add default values to concept progressses.

--- app/models/concept_progress.rb
+++ app/models/concept_progress.rb
@@ -1,6 +1,20 @@
 class ConceptProgress < ActiveRecord::Base
+  after_initialize :set_defaults
+
   belongs_to :enrollment
   belongs_to :concept
 
   validates_presence_of :type_of_error, :next_steps, :mastery_level, :p_level
+
+  private
+
+  def set_defaults
+    not_available = "N/A"
+    default_level = 1
+
+    self.type_of_error ||= not_available
+    self.next_steps ||= not_available
+    self.mastery_level ||= default_level
+    self.p_level ||= default_level
+  end
 end
--- spec/models/concept_progress_spec.rb
+++ spec/models/concept_progress_spec.rb
@@ -13,4 +13,13 @@ describe ConceptProgress do
   it { should validate_presence_of :next_steps }
   it { should validate_presence_of :mastery_level }
   it { should validate_presence_of :p_level }
+
+  describe "initial values" do
+    let(:cp) { ConceptProgress.create }
+
+    example { expect(cp.type_of_error).to eq "N/A" }
+    example { expect(cp.next_steps).to eq "N/A" }
+    example { expect(cp.mastery_level).to eq 1 }
+    example { expect(cp.p_level).to eq 1 }
+  end
 end
