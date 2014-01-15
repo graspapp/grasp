f24c025b25ba6aef8b08f70d02b1281f9450d4ec
Zach Latta <zach@zachlatta.com>
2014-01-15 13:20:00 -0800

    Remove methods that were being overrided in ApplicationPolicy.

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -5,37 +5,5 @@ class ApplicationPolicy
     @user = user
     @record = record
   end
-
-  def index?
-    false
-  end
-
-  def show?
-    scope.where(:id => record.id).exists?
-  end
-
-  def create?
-    false
-  end
-
-  def new?
-    create?
-  end
-
-  def update?
-    false
-  end
-
-  def edit?
-    update?
-  end
-
-  def destroy?
-    false
-  end
-
-  def scope
-    Pundit.policy_scope!(user, record.class)
-  end
 end
 
