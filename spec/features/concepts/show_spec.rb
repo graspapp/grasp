c043fd9f934f0246ec6503298d1628ed743b84f0
Stanley Jackson <stanjackson6@gmail.com>
2014-01-27 02:42:19 -0800

    Yet another attempt with concept progress edit

--- spec/features/concepts/show_spec.rb
+++ spec/features/concepts/show_spec.rb
@@ -47,7 +47,8 @@ describe "Concept show" do
                     find("option[value='#{concept_progress.mastery_level}']").
                     select_option
           
-          find("option[value='#{concept_progress[:type_of_error]}']").click
+          select concept_progress[:type_of_error], :from =>
+             "concept_progress[type_of_error]"
           page.find_by_id("concept_progress_next_steps").
                     find("option[value='Play in Brilliant.org']").select_option                      
           fill_in("Plan to accomplish next steps", 
