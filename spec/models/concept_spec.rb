be37e26d7d38eee4cfe9474fefeb068f9ec547bb
Stanley Jackson <stanjackson6@gmail.com>
2014-03-13 19:01:37 -0700

    Resources for parents added to Concepts, with external link capabilities.

--- spec/models/concept_spec.rb
+++ spec/models/concept_spec.rb
@@ -7,6 +7,7 @@ describe Concept do
 
   it { should respond_to :name }
   it { should respond_to :description }
+  it { should respond_to :resources}
 
   it { should validate_presence_of :name }
   it { should validate_presence_of :description }
