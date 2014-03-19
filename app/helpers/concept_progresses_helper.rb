8b1eebee473ddc371f6ab8a4566aeb55b72f79d9
Stanley Jackson <stanjackson6@gmail.com>
2014-03-18 20:50:33 -0700

    Preliminary tooltips.

--- app/helpers/concept_progresses_helper.rb
+++ app/helpers/concept_progresses_helper.rb
@@ -1,2 +1,16 @@
 module ConceptProgressesHelper
+  def short_term_goal_explanation
+    "After baseline quick check, set a goal for understanding this
+     concept to be accomplished in one week."
+  end
+  
+  def mastery_goal_explanation
+    "After a recent scored assessment, track your mastery level approved by the
+     teacher."
+   end
+   
+   def note_explanation
+     "Write yourself a note/reminder that will guide your efforts in and out of
+      class around deepening your understanding."
+    end
 end
