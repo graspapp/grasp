439be385b775acbe3fa4bac34414deee988a2750
Stanley Jackson <stanjackson6@gmail.com>
2014-03-20 17:41:20 -0700

    List concepts in the order they are created.

--- app/models/concept.rb
+++ app/models/concept.rb
@@ -1,6 +1,6 @@
 class Concept < ActiveRecord::Base
   belongs_to :unit
-
+  default_scope -> { order('created_at ASC') }
   has_many :concept_progresses, dependent: :destroy
 
   validates_presence_of :name, :description
